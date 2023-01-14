917eeeb3368ce18c295f4f27ed25fd74037399c5
jianyun2020 <sujianyun2020@163.com>
2023-01-14 13:03:48 +0800

    change material

--- src/three/mesh/city.js
+++ src/three/mesh/city.js
@@ -1,15 +1,19 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import scene from '../scene'
+import modifyCityMaterial from '../modify/modifyCityMaterial'
 
 export default function createCity() {
   const gltfLoader = new GLTFLoader()
   gltfLoader.load("./models/city.glb", (gltf) => {
     gltf.scene.traverse(item => {
       if (item.type === 'Mesh') {
-        item.material = new THREE.MeshBasicMaterial({
-          color: 0x00ffff
+        const cityMaterial = new THREE.MeshBasicMaterial({
+          color: new THREE.Color(0x0c0e6f)
         })
+    
+        item.material = cityMaterial
+        modifyCityMaterial(item)
       }
     })
     scene.add(gltf.scene)
