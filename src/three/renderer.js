81b376e065d9f102e554a8e1b16ca1df4d2aada9
jianyun2020 <sujianyun2020@163.com>
2023-01-22 15:15:22 +0800

    天空盒子和抗锯齿

--- src/three/renderer.js
+++ src/three/renderer.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three'
 
-const renderer = new THREE.WebGLRenderer()
+const renderer = new THREE.WebGLRenderer({
+  // 抗锯齿
+  antialias: true
+})
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.shadowMap.enabled = true
 
--- src/three/scene.js
+++ src/three/scene.js
@@ -3,6 +3,20 @@ import * as THREE from 'three'
 
 const scene = new THREE.Scene()
 
+// 天空盒子
+const textureCubeLoader = new THREE.CubeTextureLoader().setPath("./textures/")
+const textureCube = textureCubeLoader.load([
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg",
+])
+
+scene.background = textureCube
+scene.environment = textureCube
+
 // 通过射线检测获取坐标
 // const raycaster = new THREE.Raycaster()
 // const mouse = new THREE.Vector2()
